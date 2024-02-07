c8dd3e80fd0470735c70a05ed5a4f516c2d05956
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-07 11:11:55 -0700

    interpolated normal values

--- app/models/Car.js
+++ app/models/Car.js
@@ -24,16 +24,15 @@ export class Car {
     <div class="col-12 mb-3">
       <div class="bg-light rounded shadow-lg d-block d-md-flex">
         <img class="car-img rounded-start"
-          src="https://images.unsplash.com/photo-1625658281581-2da46feb5d1d?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="Picture of car">
+          src="${this.imgUrl}"
+          alt="Picture of ${this.make} ${this.model}">
 
         <div class="py-2 px-5">
-          <h2>2000 Dodge Mustang</h2>
-          <h2>$300000</h2>
-          <h3>200000 Miles</h3>
+          <h2>${this.year} ${this.make} ${this.model}</h2>
+          <h2>$${this.price}</h2>
+          <h3>${this.mileage} Miles</h3>
           <h4>Listed on 12/12/2012</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis veritatis doloribus ea ipsa excepturi in
-            dolor autem totam molestiae rem.</p>
+          <p>${this.description}</p>
           <div class="d-flex">
             <p>Car Color:</p>
             <div class="ms-4 car-color-circle"></div>
