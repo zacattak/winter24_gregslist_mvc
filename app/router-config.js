7ac5907f1f826033f64d8e93ff203b69b30522ad
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-07 10:52:21 -0700

    finished template in html

--- app/router-config.js
+++ app/router-config.js
@@ -5,6 +5,7 @@ import { Router } from "./utils/Router.js";
 
 export const router = new Router([
   {
+    // NOTE if url looks like 'localhost:8080', this route object is loaded
     path: '',
     view: /*html*/`
     <h1 class="m-4 display-3">Welcome to Gregslist!</h1>
@@ -12,7 +13,9 @@ export const router = new Router([
     `
   },
   {
+    // NOTE if url looks like 'localhost:8080/#/cars', this route object is loaded
     path: '#/cars',
+    // NOTE filepath to our CarsView HTML file
     view: 'app/views/CarsView.html'
   }
 ])
