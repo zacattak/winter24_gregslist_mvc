18cc4067148094495ae4b48a46328a19a85f7e5d
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-07 12:23:59 -0700

    logging car form data

--- app/controllers/CarsController.js
+++ app/controllers/CarsController.js
@@ -1,4 +1,6 @@
 import { AppState } from "../AppState.js";
+import { getFormData } from "../utils/FormHandler.js";
+import { Pop } from "../utils/Pop.js";
 import { setHTML } from "../utils/Writer.js";
 
 function _drawCars() {
@@ -17,5 +19,27 @@ export class CarsController {
   constructor () {
     console.log('Cars controller loaded');
     _drawCars()
+  }
+
+  createCar() {
+    try {
+      event.preventDefault() //NOTE stop the page from refreshing
+      console.log('creating car');
+
+      const form = event.target
+
+      console.log('targeted event', form);
+
+      // @ts-ignore
+      console.log('car make', form.make.value);
+
+      const carFormData = getFormData(form)
+
+      console.log('here is your car data object!', carFormData);
+
+    } catch (error) {
+      console.error(error);
+      Pop.error(error.message)
+    }
   }
 }
